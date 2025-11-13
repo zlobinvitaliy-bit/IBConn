096e3f489823533db7f8b1116484a953857d0312
VM-SERV\User <zlobin.vitaliy@gmail.ru>
2025-11-13 10:31:29 +0300

    Добавлена модель

---
+++ PostgresDataAccessExample/PostgresDataAccessExample/Models/TaskModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARM.Models
+{
+    public class TaskModel
+    {
+        public string Time { get; set; }
+        public string TDoc { get; set; }
+        public string Product { get; set; }
+        public string FlowDirection { get; set; }
+        public string Car { get; set; }
+        public string Tank { get; set; }
+        public string CarDriver { get; set; }
+        public string SetTotal_V { get; set; }
+        public string Fact_V { get; set; }
+        public string SetTotal_M { get; set; }
+        public string Fact_M { get; set; }
+        public string SetDensity { get; set; }
+    }
+}
---
+++ PostgresDataAccessExample/PostgresDataAccessExample/ViewModels/TaskViewModel.cs
@@ -0,0 +1,43 @@
+﻿using ARM.Models;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostgresDataAccessExample.ViewModels
+{
+    internal class TaskViewModel
+    {
+
+        public string sql = @"SELECT ""RecTime"",""Doc"", ""Product"",""Direction"", ""Machine"", ""Tank"", ""Driver"", ""DocV"", ""DocW"", ""DocD"", ""State"", ""Receipt""
+                        FROM ""JobsN"" ORDER BY ""RecTime"";";
+        public async Task<List<TaskModel>> GetTasksAsync()
+        {
+            await using var connection = await dataSource.OpenConnectionAsync();
+            
+            await using var cmd = new NpgsqlCommand(sql, connection);
+            await using var reader = await cmd.ExecuteReaderAsync();
+            var taskList = new List<TaskModel>();
+            while (await reader.ReadAsync())
+            {
+                taskList.Add(new TaskModel
+                {
+                    Time = reader.GetDateTime("RecTime").ToString("yyyy-MM-dd HH:mm:ss"),
+                    TDoc = reader.GetString("Doc"),
+                    Product = reader.GetInt16("Product").ToString(),
+                    FlowDirection = reader.GetInt16("Direction").ToString(),
+                    Car = reader.GetString("Machine"),
+                    Tank = reader.GetInt16("Tank").ToString(),
+                    CarDriver = reader.GetString("Driver"),
+                    SetTotal_V = reader.GetInt16("DocV").ToString(),
+                    Fact_V = reader.GetInt16("DocW").ToString(),
+                    SetTotal_M = reader.GetInt16("DocD").ToString(),
+                    Fact_M = reader.GetInt16("State").ToString(),
+                    SetDensity = reader.GetInt16("Receipt").ToString()
+
+                });
+            }
+            return taskList;
+        }
+    }
+}
